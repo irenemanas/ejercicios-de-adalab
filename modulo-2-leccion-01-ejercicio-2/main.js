ac460d3e5bcb81d38049503ba59970346b68a22c
irenemanas <irenemanas@gmail.com>
2021-01-18 18:24:06 +0100

    Add const

---
+++ modulo-2-leccion-01-ejercicio-2/main.js
@@ -0,0 +1,4 @@
+'use strict';
+document.querySelector('h2').innerHTML = 'Esta página no es compatible con la versión actual de tu navegador. Por favor actualízalo a la versión más reciente';
+const homeAddress = 'Calle Hermosilla 5';
+const pairAddress = 'Calle leganitos 20';
