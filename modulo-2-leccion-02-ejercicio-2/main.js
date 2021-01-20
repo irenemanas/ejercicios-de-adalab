b1e284e5a7f10687e898f12be91200513b84b839
irenemanas <irenemanas@gmail.com>
2021-01-20 10:49:11 +0100

    check console

--- modulo-2-leccion-02-ejercicio-2/main.js
+++ modulo-2-leccion-02-ejercicio-2/main.js
@@ -1,3 +1,6 @@
 const superWave = document.querySelector('.super-wave');
-superWave.classList.add('.bold');
+superWave.classList.add('bold');
 console.log(superWave);
+
+
+console.log(superWave.classList);
