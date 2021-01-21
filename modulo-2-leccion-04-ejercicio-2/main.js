1d86b441b944ad033c6a2daee3c93a9b55982fe2
irenemanas <irenemanas@gmail.com>
2021-01-21 19:32:16 +0100

    Add average functions

---
+++ modulo-2-leccion-04-ejercicio-2/main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+
+//FUNCTION 1 (AVERAGE)
+
+function average(a, b, c, d) {
+    return (a + b + c + d)/4; 
+}
+const averageResult = average (2, 2, 2, 2);
+console.log('La media es:', averageResult);
+
+//FUNCTION 2 (AVERAGE)
+
+function average2(a, b, c, d) {
+    return (a + b + c + d)/4; 
+}
+const average2Result = average (3, 5, 10, 17);
+console.log('La media es:', average2Result);
+
+//FUNCTION 3 (AVERAGE)
+
+function average3(a, b, c, d) {
+    return (a + b + c + d)/4; 
+}
+const average3Result = average (10, 15, 20, 35);
+console.log('La media es:', average3Result);
+
