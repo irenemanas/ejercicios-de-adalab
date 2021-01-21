208cf65f91074e3e22582a188f9435148dbaeb58
irenemanas <irenemanas@gmail.com>
2021-01-21 19:16:59 +0100

    Add multiply functions

---
+++ modulo-2-leccion-04-ejercicio-1/main.js
@@ -0,0 +1,22 @@
+'use strict'
+
+// FUNCTION 1
+function multiply (a, b) {
+    return a * b;
+}
+const multiplyResult = multiply(2, 8);
+console.log('Producto', multiplyResult);
+
+// FUNCTION 2
+function multiply2 (a, b) {
+    return a * b;
+}
+const multiply2Result = multiply2(9, 3);
+console.log('Producto', multiply2Result);
+
+// FUNCTION 3
+function multiply3 (a, b) {
+    return a * b;
+}
+const multiply3Result = multiply3(8, 5);
+console.log('Producto', multiply3Result);
