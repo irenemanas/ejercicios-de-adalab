de716913ecf4974385c4b321d88b660ab696526b
irenemanas <irenemanas@gmail.com>
2021-01-21 17:34:10 +0100

    Add conditional

---
+++ module-2-leccion-03-ejercicio-2/main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const amandaName = 'Amanda';
+const ireneName = 'Irene';
+const passWordName = 'Amanda'
+
+if (passWordName === amandaName || passWordName === ireneName);
+    console.log('Bienvenida: ' + amandaName);
