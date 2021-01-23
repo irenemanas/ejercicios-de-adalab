b61fe6f66fd0c8b2a5749e3a973bbf7b5be68503
irenemanas <irenemanas@gmail.com>
2021-01-23 19:21:19 +0100

    Add OddEven function exercise

--- modulo-2-leccion-04-ejercicio-4/main.js
+++ modulo-2-leccion-04-ejercicio-4/main.js
@@ -1,3 +1,23 @@
 'use strict'
 
-function 
+
+///////
+
+    const number = 15;
+    if (number % 2 === 0) {
+    console.log('El número es PAR');  
+    } else {
+        console.log('El número es IMPAR');  
+    }
+
+
+    // Intentar con una función
+
+function oddEven(y) {
+    if (y % 2 === 0) {
+        console.log('True');  
+        } else {
+            console.log('False');  
+        }
+    }
+    oddEven(9); 
