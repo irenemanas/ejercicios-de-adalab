7ca1256bb6077a07071ad37eb6bd82a3e45e3e55
irenemanas <irenemanas@gmail.com>
2021-01-24 09:26:06 +0100

    add ternary operator exercise

---
+++ module-2-leccion-03-ejercicio-6/main.js
@@ -0,0 +1,9 @@
+'use strict'
+
+// OPERADOR CONDICIONAL o TERNARIO
+
+const avocadoPrice = 1.5;
+const money = 33;
+const avocados = money >= avocadoPrice ? money / avocadoPrice : 0;
+console.log('Puedo comprar ' + money / avocadoPrice + ' aguacates');
+
