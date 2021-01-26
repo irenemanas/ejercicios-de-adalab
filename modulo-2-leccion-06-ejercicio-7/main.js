3fbd80929cd95449bf8b6e40fe6d5d813ee0da85
irenemanas <irenemanas@gmail.com>
2021-01-26 11:09:03 +0100

    change object properties exercise

---
+++ modulo-2-leccion-06-ejercicio-7/main.js
@@ -0,0 +1,24 @@
+'use strict'
+    const user = {};
+
+    const job = 'developer';
+
+    user.firstName = 'Amanda';
+    user.lastName = 'Garcia';
+    user.age = 34;
+    user.job = job;
+    
+
+    console.log(user);
+    user.firstName = 'Irene';
+    console.log(user.firstName);
+    console.log(user);
+    user.age += 1;
+    console.log(user.age);
+
+
+
+
+
+
+
